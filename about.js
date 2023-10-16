dad3ffa7259184a626b0ed51db577110d411db7d
naufalhsb <122070549+naufalhsb@users.noreply.github.com>
2023-10-16 11:34:47 +0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page content.</p>
+    </div>
+  );
+}
+
+export default About;
+
