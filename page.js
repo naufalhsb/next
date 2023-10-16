d33b96f3389e2e47a31d5a1ed691cdb7a47a7389
naufalhsb <122070549+naufalhsb@users.noreply.github.com>
2023-10-16 11:33:02 +0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Home Page</h1>
+      <p>This is the home page content.</p>
+      <Link href="/about">
+        <a>About Us</a>
+      </Link>
+    </div>
+  );
+}
+
+export default Home;
+
+
+
+
